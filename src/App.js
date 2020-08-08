2a5c8e88f17862a93fadd03c9d30ed4cb160cfbd
Yasamin Yaldaei <yasaminyaldaei@gmail.com>
2020-08-08 23:49:45 +0430

    useState, custome hooks, update ESLint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cokatiel" />
-      <Pet name="Doink" animal="cat" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
