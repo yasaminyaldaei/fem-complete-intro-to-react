d949a3189f94ad825e68da27d7684a9ec35344a5
Yasamin Yaldaei <yasaminyaldaei@gmail.com>
2020-08-27 14:00:27 +0430

    add carousel

--- src/Details.js
+++ src/Details.js
@@ -1,33 +1,34 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
 class Details extends React.Component {
-  state = { loading: true }
+  state = { loading: true };
 
   componentDidMount() {
-    pet.animal(this.props.id)
-      .then(({ animal }) => {
-        this.setState({
-          name: animal.name,
-          animal: animal.type,
-          location: `${animal.contact.address.city} , ${animal.contact.address.state}`,
-          description: animal.description,
-          media: animal.photos,
-          breed: animal.breeds.primary,
-          loading: false,
-        })
-      }, console.error)
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city} , ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    }, console.error);
   }
   render() {
     if (this.state.loading) {
-      return <h1>Loading...</h1>
+      return <h1>Loading...</h1>;
     }
 
-    const { animal, breed, location, description, name } = this.state;
+    const { animal, breed, location, description, name, media } = this.state;
 
     return (
       <div>
         <div className="details">
+          <Carousel media={media} />
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
           <button>Adopt {name}</button>
