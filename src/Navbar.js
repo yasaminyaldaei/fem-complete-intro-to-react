9a2f99370534ecea01ab0af451dc78ec238fd1ea
Yasamin Yaldaei <yasaminyaldaei@gmail.com>
2020-10-02 20:36:29 +0330

    add design system, add animation

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import { Link } from "@reach/router";
-import { css } from "@emotion/core";
+import { css, keyframes } from "@emotion/core";
+import colors from "./colors";
+
+const spin = keyframes`
+    to {
+        transform: rotate(360deg)
+    }
+`
 
 const Navbar = () => {
 
@@ -10,14 +17,20 @@ const Navbar = () => {
         <header
             onClick={() => setPadding(padding + 15)}
             css={css`
-        background-color: #333;
-        padding: ${padding}px;
-    `}
+                background-color: ${colors.primary};
+                padding: ${padding}px;
+            `}
         >
             <Link to="/">Adopt Me!</Link>
             <span
                 css={css`
                         font-size: 60px;
+                        display: inline-block;
+                        animation: 1s ${spin} linear infinite;
+
+                        &:hover {
+                            text-decoration: underline
+                        }
                 `}
                 role="img" aria-label="logo">😺️</span>
         </header>
